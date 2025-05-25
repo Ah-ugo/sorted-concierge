361d421e572c14ee20d665c75ee7287d58164d6a
mac <ahuekweprinceugo@gmail.com>
2025-05-25 05:45:41 +0100

    another commit

--- app/page.tsx
+++ app/page.tsx
@@ -165,7 +165,7 @@ export default function Home() {
             playsInline
             preload="auto"
           >
-            <source src="/hero-bg2.mp4" type="video/mp4" />
+            <source src="/new-hero.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
 
